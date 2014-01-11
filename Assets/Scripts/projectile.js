280d33946f7f3ad730417ce6999a135e61ddfbb9
Jon Wiedmann <jonwiedmann@gmail.com>
2014-01-10 16:03:03 -0800

    init commit

---
+++ Assets/Scripts/projectile.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var moveUp : KeyCode;
+var moveDown : KeyCode;
+
+var dtY : int = 20;
+
+function Update () {
+	if(Input.GetKey(moveUp)){
+		// move position
+	}
+	else if(Input.GetKey(moveDown)){
+		// move down
+	}
+}
