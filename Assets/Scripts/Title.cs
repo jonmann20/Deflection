8ca86e64781a45801a7c7e5a78269e05386d3bd1
Jon Wiedmann <jonwiedmann@gmail.com>
2014-02-14 19:37:24 -0500

    real gui layout

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+	
+	void Update(){
+        if(Input.GetKey(KeyCode.Return)) {
+            Application.LoadLevel("main");
+        }
+	}
+
+    void OnGUI(){
+        Utils.that.scaleGUI();
+
+        GUIContent titleContent = new GUIContent("tyBomber");
+        GUIStyle titleStyle = new GUIStyle();
+        titleStyle.fontSize = 86;
+        titleStyle.normal.textColor = Color.white;
+        Vector2 titleSize = titleStyle.CalcSize(titleContent);
+
+        GUIContent ctaContent = new GUIContent("Press Enter");
+        GUIStyle ctaStyle = new GUIStyle();
+        ctaStyle.fontSize = 38;
+        ctaStyle.normal.textColor = Color.white;
+        Vector2 ctaSize = ctaStyle.CalcSize(ctaContent);
+        
+
+        GUI.Label(new Rect(Utils.HALFW - titleSize.x/2, Utils.FULLH/5 - titleSize.y, titleSize.x, titleSize.y), titleContent, titleStyle);
+        GUI.Label(new Rect(Utils.HALFW - ctaSize.x/2, Utils.FULLH/1.8f - ctaSize.y, ctaSize.x, ctaSize.y), ctaContent, ctaStyle);
+    }
+}
+
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utils : MonoBehaviour {
+    public static Utils that;
+
+    public const float FULLW = 1920;
+    public const float FULLH = 1080;
+
+    public const float HALFW = FULLW / 2;
+    public const float HALFH = FULLH / 2;
+
+    void Awake(){
+        that = this;
+    }
+
+    public void scaleGUI(){
+        float rx = Screen.width / FULLW;
+        float ry = Screen.height / FULLH;
+
+        GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(rx, ry, 1)); 
+    }
+}
